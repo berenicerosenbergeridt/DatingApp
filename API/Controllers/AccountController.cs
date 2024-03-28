5fc315b5cf92d34149378bd3a0cc7eb7097f6e69
berenicerosenbergeridt <brosenberger@idt.pf>
2024-03-28 11:46:41 -1000

    section 6

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -64,7 +64,7 @@ namespace API.Controllers
 
             for (int i = 0 ; i < computedHash.Length; i++)
             {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("ton mdp est faux!");
             }
 
             return new UserDto

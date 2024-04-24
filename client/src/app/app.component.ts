5d1d48a5a2fc63189f2c6935d569aa72da1b8a1e
berenicerosenbergeridt <brosenberger@idt.pf>
2024-04-24 09:57:32 -1000

    fin section 10

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from './_services/account.service';
 import { User } from './_models/user';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',

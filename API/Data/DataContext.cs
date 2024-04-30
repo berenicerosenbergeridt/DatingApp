9544f9ff59ef88783e0c54bb24d6cb935e0628af
berenicerosenbergeridt <brosenberger@idt.pf>
2024-04-29 16:54:07 -1000

    erreur a corriger

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -12,5 +12,25 @@ public class DataContext : DbContext //créer l'instance
     {
     }
 
-    public DbSet<AppUser> Users {get;set;} //"users" = nom de la table dans la base de données
+    public DbSet<AppUser> Users { get; set; } //"users" = nom de la table dans la base de données
+    public DbSet<UserLike> Likes { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<UserLike>()
+            .HasKey(k=> new {k.SourceUserId, k.TargentUserId});
+
+        builder.Entity<UserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikedUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<UserLike>()
+            .HasOne(s => s.TargetUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.TargentUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int targntUserId);
+        Task<AppUser> GetUserWithLikes(int userId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+    }
+}
